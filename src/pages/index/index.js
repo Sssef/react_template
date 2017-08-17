957564c7784375afbc9d0f8e75152d5e07053808
Sergey Smirnov <sssmirnov@hotmail.com>
2017-08-17 18:07:15 +0700

    rebuild

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,9 +2,6 @@ import './index.sass';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../../components/App';
-import '../../libs/bootstrap4/bootstrap.min.css';
-//import '../../libs/bootstrap4/bootstrap.min.js';
-// import '../../libs/bootstrap4/tether.min.js';
 
 ReactDOM.render(
     <App />,
